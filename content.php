3284cf0a664c6ce0e06708cb65e81e0181c9dd63
Grinch05 <151930736+Grinch05@users.noreply.github.com>
2024-02-21 17:16:28 +0300

    Update content.php

--- content.php
+++ content.php
@@ -5,77 +5,78 @@
             $content= $_GET['content'];
             include "$content";
         } 
-        else{
+         else {
             if(isset($_GET['region'])){
                 $selectedRegion = (int)$_GET['region'];
                 $mysql = "SELECT city.*, region.name AS region_name
-                FROM city
-                JOIN region ON city.region = region.id
-                WHERE city.region = $selectedRegion 
-               ";
+                          FROM city
+                          JOIN region ON city.region = region.id
+                          WHERE city.region = $selectedRegion";
                 $result = $dbcon->query($mysql);
                 $city_data = $result->fetch_all(MYSQLI_ASSOC);
-                    
-                    $myrow = $result->fetch_assoc();
-                        $_SESSION["photo"] = $myrow['logo'];
-                          ?>
-                    
-                    <!-- // Вывод таблицы -->
+                
+                // Проверяем, есть ли данные в результирующем наборе
+                if (!empty($city_data)) {
+                    ?>
+                    <!-- Вывод таблицы -->
                     <div class="tablGL">
-                    <div class="tabl">
+                        <div class="tabl">
+                            <table>
+                                <?php
+                                foreach ($city_data as $row) {
+                                    $_SESSION["photo"] = $row['logo'];
+                                    echo "<tr><th>Лого</th>
+                                          <th><a class='link' href='?content=city.php&city=" . $row['id'] . "'>" . $row['name'] . "</a></th>
+                                          <th>" . $row['region_name'] . "</th></tr>";
+                                    echo "<td><img src='./img/" . $_SESSION['photo'] . "'></td>";
+                                    echo "<td colspan='2'>" . $row['description'] . "</td>";
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
                     <?php
-                    echo "<table >";
-                   
-                    
-                    foreach ($result as $row) { 
-                        $_SESSION["photo"] = $row['logo'];
-                        echo "<tr><th >Лого</th>
-                        <th > <a class='link' href='?content=city.php&city=" . $row['id'] . "'>" . $row['name'] . "</a></th>
-                        <th >". $row['region_name'] ."</th></tr>";
-                        echo "<td ><img src='./img/" . $_SESSION['photo'] . "'></td>";
-                        echo "<td  colspan='2'>" . $row['description'] . "</td>";
-                    }    
-                    echo "</table>";   
-                         
-                    ?></div></div><?php    
+                } else {
+                    // Если данных нет, выводим сообщение
+                    echo "Нет данных для отображения.";
+                }
+            } else {
+                // Если параметр region не установлен
+                $mysql = "SELECT city.*, region.name AS region_name
+                          FROM city
+                          JOIN region ON city.region = region.id
+                          ORDER BY `id` DESC LIMIT 5";
+                $result = $dbcon->query($mysql);
+                $city_data = $result->fetch_all(MYSQLI_ASSOC);
+                
+                // Проверяем, есть ли данные в результирующем наборе
+                if (!empty($city_data)) {
+                    ?>
+                    <!-- Вывод таблицы -->
+                    <div class="tablGL">
+                        <div class="tabl">
+                            <table>
+                                <?php
+                                foreach ($city_data as $row) {
+                                    $_SESSION["photo"] = $row['logo'];
+                                    echo "<tr><th>Лого</th>
+                                          <th><a class='link' href='?content=city.php&city=" . $row['id'] . "'>" . $row['name'] . "</a></th>
+                                          <th>" . $row['region_name'] . "</th></tr>";
+                                    echo "<td><img src='./img/" . $_SESSION['photo'] . "'></td>";
+                                    echo "<td colspan='2'>" . $row['description'] . "</td>";
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+                    <?php
+                } else {
+                    // Если данных нет, выводим сообщение
+                    echo "Нет данных для отображения.";
+                }
             }
-            else{
-              $mysql = "SELECT city.*, region.name AS region_name
-            FROM city
-            JOIN region ON city.region = region.id
-            ORDER BY `id` DESC LIMIT 5";
-            $result = $dbcon->query($mysql);
-            $city_data = $result->fetch_all(MYSQLI_ASSOC);
-                
-                $myrow = $result->fetch_assoc();
-                    $_SESSION["photo"] = $myrow['logo'];
-                      ?>
-                
-                <!-- // Вывод таблицы -->
-                <div class="tablGL">
-                <div class="tabl">
-                <?php
-                echo "<table >";
-               
-                
-                foreach ($result as $row) { 
-                    $_SESSION["photo"] = $row['logo'];
-                    $_SESSION["city"] = $row['name'];
-                    echo "<tr><th >Лого</th>
-                
-                    <th > <a class='link' href='?content=city.php&city=" . $row['id'] . "'>" . $row['name'] . "</a></th>
-                    <th >". $row['region_name'] ."</th></tr>";
-                    echo "<td ><img src='./img/" . $_SESSION['photo'] . "'></td>";
-                    echo "<td  colspan='2'>" . $row['description'] . "</td>";
-                   
-                }    
-                echo "</table>";   
-                     
-                ?></div></div><?php  
-            }
-              
-            
-            } 
+        }
+        
         
             ?> 
                 
